9c5aa2a16aaf53258935bb940d8418eb4a296035
ignatius-n <ignatius.freelacer@gmail.com>
2021-10-08 10:13:04 +0300

    improved digital products(products with resources located at digital-angel) feature

--- core/resources/views/front/bookworm/product/v3.blade.php
+++ core/resources/views/front/bookworm/product/v3.blade.php
@@ -42,7 +42,7 @@
 
                                 <div class="px-3 d-flex mb-5 align-items-center">
                                     @if ($product->digital)
-                                        <a href="{{ $be->digital_resource_link }}" class="btn btn-dark border-0 rounded-0 p-3 min-width-250 ml-md-4 single_add_to_cart_button button alt cart-btn cart-link" style="color: #fff" data-toggle="modal" data-target="#productInquiryModal">
+                                        <a href="{{ $be->digital_resource_link }}" class="btn btn-dark border-0 rounded-0 p-3 min-width-250 ml-md-4 single_add_to_cart_button button alt cart-btn cart-link" style="color: #fff">
                                             {{ $be->digital_resource_text }}
                                         </a>
                                     @elseif($product->offline)
--- core/resources/views/front/bookworm/product/v4.blade.php
+++ core/resources/views/front/bookworm/product/v4.blade.php
@@ -42,7 +42,7 @@
                                         <div class="mb-4">
                                         </div>
                                         @if ($product->digital)
-                                            <a href="{{ $be->digital_resource_link }}" class="btn btn-dark border-0 rounded-0 p-3 min-width-250 ml-md-4 single_add_to_cart_button button alt cart-btn cart-link" style="color: #fff" data-toggle="modal" data-target="#productInquiryModal">
+                                            <a href="{{ $be->digital_resource_link }}" class="btn btn-dark border-0 rounded-0 p-3 min-width-250 ml-md-4 single_add_to_cart_button button alt cart-btn cart-link" style="color: #fff">
                                                 {{ $be->digital_resource_text }}
                                             </a>
                                         @elseif($product->offline)
