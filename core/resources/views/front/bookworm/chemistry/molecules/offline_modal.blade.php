a3e7b30a584e608a655a4812f7fe9d3fe3c71a1e
ignatius-n <ignatius.freelacer@gmail.com>
2021-11-04 16:57:59 +0300

    updated inquiry forms elements alignment and other extra corrections

--- core/resources/views/front/bookworm/chemistry/molecules/offline_modal.blade.php
+++ core/resources/views/front/bookworm/chemistry/molecules/offline_modal.blade.php
@@ -85,7 +85,10 @@
                                     </div>
                                 </div>
                                 @if ($bs->is_recaptcha == 1)
-                                    <div class="col-md-12 my-2">
+                                    <style>
+                                        .captcha-col > div { display: inline-block; }
+                                    </style>
+                                    <div class="col-md-12 my-2 text-center captcha-col">
                                         {!! NoCaptcha::renderJs() !!}
                                         {!! NoCaptcha::display() !!}
                                         @if ($errors->has('g-recaptcha-response'))
@@ -99,7 +102,7 @@
                             </div>
                         </form>
                     </div>
-                    <div class="modal-footer">
+                    <div class="modal-footer text-center">
                         {{--                                                        <button type="button" class="btn btn-secondary py-3" data-dismiss="modal">Close</button>--}}
                         <button type="button" class="btn btn-dark submit-button border-0 rounded-0 p-3 min-width-250 ml-md-4 single_add_to_cart_button button alt cart-btn cart-link" style="color: #fff">{{__('Submit')}}</button>
                         {{--                                                        <input class="py-3" type="submit" value="{{__('Submit')}}">--}}
