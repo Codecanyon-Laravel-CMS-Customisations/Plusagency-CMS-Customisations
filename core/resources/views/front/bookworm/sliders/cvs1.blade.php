dbf754c2e34298a5eae5a1bdb13b327fa85d6e50
ignatius-n <ignatius.freelacer@gmail.com>
2021-11-06 20:35:24 +0300

    corrected some minor bugs in video video slider feature

--- core/resources/views/front/bookworm/sliders/cvs1.blade.php
+++ core/resources/views/front/bookworm/sliders/cvs1.blade.php
@@ -1,3 +1,6 @@
+@php
+    $vid    = str_ireplace('https://www.youtube.com/watch?v=', '', $bs->hero_section_video_link);
+@endphp
 <link href="{{ asset('assets/front/video-slider/dist/css/jquery.mb.YTPlayer.min.css') }}" rel="stylesheet" />
 <div class="hero-slider-with-banners space-bottom-2 mt-4d875">
     <div class="container">
@@ -5,7 +8,7 @@
             <div class="col-wd-9 mb-4 mb-xl-0">
                 <div class="player w-100"
                     style="min-height: 250px !important"
-                    data-property="{videoURL:'NccfVQ0HKd4',containment:'self',autoPlay:true, mute:true, startAt:0, ratio:'16/9', opacity:1}">
+                    data-property="{videoURL:'{{ $vid }}',containment:'self',autoPlay:true, mute:true, startAt:0, ratio:'16/9', opacity:1}">
                     {{-- <i class="fas fa-spinner fa-spin fa-pulse "></i> --}}
                 </div>
             </div>
