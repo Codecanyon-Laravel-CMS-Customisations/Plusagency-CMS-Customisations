db8a7ca607046af10a616100f5c93bf07acfe0ee
ignatius-n <ignatius.freelacer@gmail.com>
2021-11-08 16:57:30 +0300

    corrected some minor bugs in video video slider feature (changed the script)

--- core/resources/views/front/bookworm/sliders/cvs1.blade.php
+++ core/resources/views/front/bookworm/sliders/cvs1.blade.php
@@ -1,23 +1,30 @@
 @php
     $vid    = str_ireplace('https://www.youtube.com/watch?v=', '', $bs->hero_section_video_link);
 @endphp
-<link href="{{ asset('assets/front/video-slider/dist/css/jquery.mb.YTPlayer.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/front/video-slider/dist/jquery.vidbacking.css') }}" type="text/css">
+<style>
+    .hero-slider-with-banners :is(iframe, video){
+        aspect-ratio: 16/9;
+        width: 100% !important;
+        height: auto !important;
+    }
+</style>
 <div class="hero-slider-with-banners space-bottom-2 mt-4d875">
     <div class="container">
         <div class="row">
             <div class="col-wd-9 mb-4 mb-xl-0">
-                <div class="player w-100"
-                    style="min-height: 250px !important"
-                    data-property="{videoURL:'{{ $vid }}',containment:'self',autoPlay:true, mute:true, startAt:0, ratio:'16/9', opacity:1}">
-                    {{-- <i class="fas fa-spinner fa-spin fa-pulse "></i> --}}
-                </div>
+                @if(\Illuminate\Support\Str::contains($bs->hero_section_video_link, 'youtube.com'))
+                    <iframe src="https://www.youtube.com/embed/{{ $vid }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen  class="vidbacking"></iframe><br type="_moz">
+                @else
+                    <video poster="poster.jpg" autoplay muted loop class="w-100">
+                        <source src="{{ $bs->hero_section_video_link }}" type="video/mp4">
+                    </video>
+                @endif
             </div>
         </div>
     </div>
 </div>
-<script src="{{ asset('assets/front/video-slider/dist/jquery.mb.YTPlayer.min.js') }}"></script>
+<script src="{{ asset('assets/front/video-slider/dist/jquery.vidbacking.js') }}"></script>
 <script>
-    jQuery(function() {
-        jQuery(".player").YTPlayer();
-    });
+    $('.hero-slider-with-banners').vidbacking();
 </script>
