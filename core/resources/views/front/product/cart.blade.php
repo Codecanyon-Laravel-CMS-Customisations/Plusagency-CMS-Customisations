4da37cdca7e97f10cdd4eb85702bdc31effe1e38
ignatius-n <ignatius.freelacer@gmail.com>
2021-11-10 11:24:24 +0300

    made a sixth update currency + country settings to the front-end (added it to shopping cart page)

--- core/resources/views/front/product/cart.blade.php
+++ core/resources/views/front/product/cart.blade.php
@@ -1,4 +1,36 @@
 @extends("front.$version.layout")
+@php
+    // $bex->base_currency_symbol          = "AI";
+    // $bex->base_currency_symbol_position = strtolower("Left");
+    // $bex->base_currency_text            = "United States Dollar";
+    // $bex->base_currency_text_position   = strtolower("Left");
+    // $bex->base_currency_rate            = "1.00";
+
+
+
+    // echo json_encode($bex);
+    // return;
+
+    $geo_data_base_currency             = angel_get_base_currency_id();//App\Models\Currency::find(81);
+    $geo_data_user_currency             = angel_get_user_currency_id();//App\Models\Currency::find(23);
+
+    // dd( $geo_data_base_currency);
+    // echo json_encode( $geo_data_base_currency);return;
+    // $bc_id      = App\Models\Currency::query()->where('name', App\BasicExtra::first()->base_currency_text)->orderBy('id', 'desc')->first();
+    // echo json_encode($bc_id);//        return $bc_id->id;
+
+
+    $bex_user_currency                  = App\Models\Currency::find($geo_data_user_currency);
+    $bex->base_currency_symbol          = $bex_user_currency->symbol;
+    $bex->base_currency_symbol_position = strtolower($bex_user_currency->symbol_position) == 'l'?  'left' : 'right';
+    $bex->base_currency_text            = $bex_user_currency->name;
+    $bex->base_currency_text_position   = strtolower($bex_user_currency->text_position) == 'l'?  'left' : 'right';
+
+    // echo json_encode($bex);return;
+    // echo json_encode(session()->all());return;
+
+
+@endphp
 
 @section('pagename')
  -
@@ -92,8 +124,8 @@
                                         @endif
                                     </div>
                                 </td>
-                                <td class="price cart_price">{{$bex->base_currency_symbol_position == 'left' ? $bex->base_currency_symbol : ''}} <span>{{$item['price']}}</span> {{$bex->base_currency_symbol_position == 'right' ? $bex->base_currency_symbol : ''}}</td>
-                                <td class="sub-total">{{$bex->base_currency_symbol_position == 'left' ? $bex->base_currency_symbol : ''}} <span>{{$item['qty'] * $item['price']}}</span> {{$bex->base_currency_symbol_position == 'right' ? $bex->base_currency_symbol : ''}}</td>
+                                <td class="price cart_price">{{$bex->base_currency_symbol_position == 'left' ? $bex->base_currency_symbol : ''}} <span>{{ isset($pvariation) ? angel_auto_convert_currency($pvariation->current_price, $geo_data_base_currency, $geo_data_user_currency) : angel_auto_convert_currency($product->current_price, $geo_data_base_currency, $geo_data_user_currency) }}</span> {{$bex->base_currency_symbol_position == 'right' ? $bex->base_currency_symbol : ''}}</td>
+                                <td class="sub-total">{{$bex->base_currency_symbol_position == 'left' ? $bex->base_currency_symbol : ''}} <span>{{ isset($pvariation) ? angel_auto_convert_currency($item['qty'] * $item['price'], $geo_data_base_currency, $geo_data_user_currency) : angel_auto_convert_currency($item['qty'] * $item['price'], $geo_data_base_currency, $geo_data_user_currency) }}</span> {{$bex->base_currency_symbol_position == 'right' ? $bex->base_currency_symbol : ''}}</td>
                                 <td>
                                     <div class="remove">
                                         <div class="checkbox">
