e2f0f8e114a41fb2013802b18218e429d9999a5f
ignatius-n <ignatius.freelacer@gmail.com>
2021-11-15 11:04:02 +0300

    improved some UI/UX on zoom buttons by adding some interactivity

--- core/resources/views/front/bookworm/product.blade.php
+++ core/resources/views/front/bookworm/product.blade.php
@@ -398,18 +398,38 @@ if (isset($_GET['variation'])) {
             var scale = 1;
             $('.mag1').on('click', function() {
                 scale = 1;
+                $(this).removeClass('btn-dark')
+                .addClass('btn-primary');
+                $(this).siblings('button').removeClass('btn-primary')
+                .addClass('btn-dark');
             });
             $('.mag2').on('click', function() {
                 scale = 2;
+                $(this).removeClass('btn-dark')
+                .addClass('btn-primary');
+                $(this).siblings('button').removeClass('btn-primary')
+                .addClass('btn-dark');
             });
             $('.mag3').on('click', function() {
                 scale = 3;
+                $(this).removeClass('btn-dark')
+                .addClass('btn-primary');
+                $(this).siblings('button').removeClass('btn-primary')
+                .addClass('btn-dark');
             });
             $('.mag4').on('click', function() {
                 scale = 4;
+                $(this).removeClass('btn-dark')
+                .addClass('btn-primary');
+                $(this).siblings('button').removeClass('btn-primary')
+                .addClass('btn-dark');
             });
             $('.mag5').on('click', function() {
                 scale = 5;
+                $(this).removeClass('btn-dark')
+                .addClass('btn-primary');
+                $(this).siblings('button').removeClass('btn-primary')
+                .addClass('btn-dark');
             });
             $(".img-blowup").on('mouseenter', function(evt) {
                 $(this).blowup({
