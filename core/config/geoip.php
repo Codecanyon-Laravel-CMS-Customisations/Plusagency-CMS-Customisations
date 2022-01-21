021d0f92637ce757e7f648fd65e32db87b7f857d
ignatius-n <ignatius.freelacer@gmail.com>
2022-01-21 16:35:06 +0300

    tests

--- core/config/geoip.php
+++ core/config/geoip.php
@@ -4,5 +4,7 @@ return [
 
   'api_key' => env('GEOIP_KEY', 'your geoip api key'),
 
-  'url'     => env('GEOIP_URL', 'https://api.freegeoip.app/json/?apikey='),
+  'url'     => env('GEOIP_URL', 'https://api.freegeoip.app/json/'),
+
+  'url2'    => env('GEOIP_URL', 'https://api.freegeoip.app/json/?apikey='),
 ];
