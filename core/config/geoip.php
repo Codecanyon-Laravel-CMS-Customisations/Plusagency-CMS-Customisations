c3fe4bef63c5d5c1afaebb223a088f7c55884781
ignatius-n <ignatius.freelacer@gmail.com>
2022-01-21 16:43:04 +0300

    made some fixes on the currency module configuration

--- core/config/geoip.php
+++ core/config/geoip.php
@@ -3,8 +3,6 @@
 return [
 
   'api_key' => env('GEOIP_KEY', 'your geoip api key'),
-
   'url'     => env('GEOIP_URL', 'https://api.freegeoip.app/json/'),
-
   'url2'    => env('GEOIP_URL', 'https://api.freegeoip.app/json/?apikey='),
 ];
