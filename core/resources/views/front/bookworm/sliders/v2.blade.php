d7c6d721f69cce6ce4d2a46646171069c35f0ef7
Zeeshan123123 <zeeshanniaz736@gmail.com>
2022-04-06 08:17:06 -0700

    Text color from backend fixed

--- core/resources/views/front/bookworm/sliders/v2.blade.php
+++ core/resources/views/front/bookworm/sliders/v2.blade.php
@@ -5,11 +5,15 @@ $lang = App\Language::where('code', $language_id)->first();
 $sliders_v2 = App\Models\SliderV2::where('language_id', $lang->id)->orderBy('id', 'ASC')->get();
 
 $bg_color = App\WebsiteColors::find(199);
+$text_color = App\WebsiteColors::find(192);
 
 if( !$bg_color ) {
-    $bg_color = App\WebsiteColors::where('element', '=', '.abh-hero-slider-v2 .abh-hero-slider-v2-main .hero_title--1')->first();
+    $bg_color = App\WebsiteColors::where(['element' => '.abh-hero-slider-v2 .abh-hero-slider-v2-main .hero_title--1', 'attribute' => 'background-color'])->first();
 }
 
+if( !$text_color ) {
+    $text_color = App\WebsiteColors::where(['element' => '.abh-hero-slider-v2 .abh-hero-slider-v2-main .hero_title--1', 'attribute' => 'color'])->first();
+}
 @endphp
 
 
@@ -27,11 +31,17 @@ if( !$bg_color ) {
                                     <div class="hero__body media-body align-self-center mb-4 mb-xl-0">
                                         <h2 class="hero__title font-size-10 mb-3" data-scs-animation-in="fadeInUp" data-scs-animation-delay="300">
                                             <span class="hero__title--1 font-weight-bold d-block" 
-                                                @if(!empty($slider->title_font_size)) style="font-size: {{ $slider->title_font_size }} !important; color: #fff0ce !important;"
+                                                @if(!empty($slider->title_font_size)) style="font-size: {{ $slider->title_font_size }} !important; 
+                                                <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'#fff0ce !important'; ?>"
+                                                @else
+                                                style="<?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :''; ?>"
                                                 @endif>{{ $slider->title }}
                                             </span>
                                             <span class="hero__title--2 d-block font-weight-normal" 
-                                                @if(!empty($slider->text_font_size)) style="font-size: {{ $slider->text_font_size }} !important; color: #fff0ce !important"
+                                                @if(!empty($slider->text_font_size)) style="font-size: {{ $slider->text_font_size }} !important; 
+                                                <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'color: #fff0ce !important'; ?>"
+                                                @else
+                                                style="<?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :''; ?>"
                                                 @endif>{{ $slider->text }}
                                             </span>
                                         </h2>
@@ -58,8 +68,8 @@ if( !$bg_color ) {
                     <div class="slider-banner flex-grow-1 mr-md-3 mr-xl-0 bg-gray-200 mb-4d875 position-relative overflow-hidden abh-hero-slider-v2-s1" style="height:250px;  <?php echo ($bg_color && $bg_color->value )? 'background: #'.$bg_color->value.' !important' :''; ?>">
                         <div class="z-index-2 position-absolute" style="top:2em; left:2em;">
                             <h2 class="slider-banner__title font-size-4 text-lh-md ">
-                                <span class="slider-banner__title--1 d-block font-weight-bold">{{ $sliders_v2->where('slider_category', 'side1')->first()->title }}</span>
-                                <span class="slider-banner__title--2 d-block">{{ $sliders_v2->where('slider_category', 'side1')->first()->text }}</span>
+                                <span class="slider-banner__title--1 d-block font-weight-bold" style=" <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'color: #fff0ce !important'; ?>">{{ $sliders_v2->where('slider_category', 'side1')->first()->title }}</span>
+                                <span class="slider-banner__title--2 d-block" style=" <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'color: #fff0ce !important'; ?>">{{ $sliders_v2->where('slider_category', 'side1')->first()->text }}</span>
 
                                 @if (!empty($slider->button_text))
                                 <a href="{{ $sliders_v2->where('slider_category', 'side1')->first()->button_url }}" class="hero__btn btn mt-2" data-scs-animation-in="fadeInUp" data-scs-animation-delay="500" style="background:#D55534; color: #fffcoe; <?php echo ($sliders_v2->where('slider_category', 'side1')->first()->button_text_font_size)?'font-size:'.$sliders_v2->where('slider_category', 'side1')->first()->button_text_font_size.'px !important;':''; ?>">
@@ -75,8 +85,8 @@ if( !$bg_color ) {
                     <div class="slider-banner flex-grow-1 ml-md-3 ml-xl-0 bg-gray-200 position-relative overflow-hidden abh-hero-slider-v2-s2" style="height:250px; <?php echo ($bg_color && $bg_color->value )? 'background: #'.$bg_color->value.' !important' :''; ?> ">
                         <div class="z-index-2 position-absolute" style="top:2em; left:2em;">
                             <h2 class="slider-banner__title font-size-4 text-lh-md">
-                                <span class="slider-banner__title--1 d-block font-weight-bold">{{ $sliders_v2->where('slider_category', 'side2')->first()->title }}</span>
-                                <span class="slider-banner__title--2 d-block">{{ $sliders_v2->where('slider_category', 'side2')->first()->text }}</span>
+                                <span class="slider-banner__title--1 d-block font-weight-bold" style=" <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'color: #fff0ce !important'; ?>">{{ $sliders_v2->where('slider_category', 'side2')->first()->title }}</span>
+                                <span class="slider-banner__title--2 d-block" style=" <?php echo ($text_color && $text_color->value )? 'color: #'.$text_color->value.' !important' :'color: #fff0ce !important'; ?>  ">{{ $sliders_v2->where('slider_category', 'side2')->first()->text }}</span>
 
                                 @if (!empty($slider->button_text))
                                 <a href="{{ $sliders_v2->where('slider_category', 'side2')->first()->button_url }}" class="hero__btn btn mt-2" data-scs-animation-in="fadeInUp" data-scs-animation-delay="500" style="background:#D55534; color: #fffcoe; <?php echo ($sliders_v2->where('slider_category', 'side2')->first()->button_text_font_size)?'font-size:'.$sliders_v2->where('slider_category', 'side2')->first()->button_text_font_size.'px !important;':''; ?>">
