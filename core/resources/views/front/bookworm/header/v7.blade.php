70e9b8f478353926cafceebcf157ae18bb23987d
muzammalrahim <creative.joomdev@gmail.com>
2022-04-19 23:07:37 +0500

    added dropdown in search section

--- core/resources/views/front/bookworm/header/v7.blade.php
+++ core/resources/views/front/bookworm/header/v7.blade.php
@@ -277,10 +277,55 @@ $searches = \App\Product::query()
                                         @endforeach
                                     </select>
                                 </div>
-                                <input type="text" class="form-control border-right-0 px-3" placeholder="Search for books by keyword" aria-label="Amount (to the nearest dollar)" id="search" onkeydown="if(event.key === 'Enter') window.location.href = `/products?search=${document.querySelector('#search').value}&minprice=0&maxprice=500.00&category_id=${document.querySelector('#category_id option:checked').value}&type=new&tag=&review=`;" value="{{ isset($_GET['search']) ? $_GET['search'] : '' }}">
-                                <div class="input-group-append border-left">
-                                    <button class="btn btn-dark btn-search px-3 rounded-0 py-2" type="button" onclick="window.location.href = `/products?search=${document.querySelector('#search').value}&minprice=0&maxprice=500.00&category_id=${document.querySelector('#category_id option:checked').value}&type=new&tag=&review=`" style="cursor: pointer;"><i class="mx-1 glph-icon flaticon-loupe "></i></button>
+
+
+
+                                
+                                {{-- commented for adding search option in products --}}
+                                {{-- <input type="text" class="form-control border-right-0 px-3" placeholder="Search for books by keyword" aria-label="Amount (to the nearest dollar)" id="search" onkeydown="if(event.key === 'Enter') window.location.href = `/products?search=${document.querySelector('#search').value}&minprice=0&maxprice=500.00&category_id=${document.querySelector('#category_id option:checked').value}&type=new&tag=&review=`;" value="{{ isset($_GET['search']) ? $_GET['search'] : '' }}"> --}}
+
+                                {{-- <select class="form-control border-right-0 px-3">
+                                    
+                                    <option> Search for books by keyword </option>
+
+                                    @foreach ($searches as $product)
+                                        <option> {{ $product->title }} </option>
+                                    @endforeach
+                                </select> --}}
+
+
+                                <div class="dropdown">
+                                  {{-- <button onclick="myFunction()" class="dropbtn">Dropdown</button> --}}
+                                  <div id="myDropdown" class="dropdown-content show w-50">
+                                    <input type="text" placeholder="Search.." id="myInput" onblur="addingClass()" onfocus="removingClass()" onkeyup="filterFunction()">
+
+                                    <div class="titles d-none"  style="height: 300px;">
+                                    @foreach ($searches as $product)
+                                            
+                                        <a>   {{ $product->title }} </a>
+
+                                    @endforeach
+                                    </div>
+
+                                    {{-- <a href="#about">About</a>
+                                    <a href="#base">Base</a>
+                                    <a href="#blog">Blog</a>
+                                    <a href="#contact">Contact</a>
+                                    <a href="#custom">Custom</a>
+                                    <a href="#support">Support</a>
+                                    <a href="#tools">Tools</a> --}}
+                                  </div>
                                 </div>
+
+
+                                
+
+
+
+
+                                {{-- <div class="input-group-append border-left">
+                                    <button class="btn btn-dark btn-search px-3 rounded-0 py-2" type="button" onclick="window.location.href = `/products?search=${document.querySelector('#search').value}&minprice=0&maxprice=500.00&category_id=${document.querySelector('#category_id option:checked').value}&type=new&tag=&review=`" style="cursor: pointer;"><i class="mx-1 glph-icon flaticon-loupe "></i></button>
+                                </div> --}}
                             </div>
                         </div>
                     </div>
@@ -874,3 +919,93 @@ $searches = \App\Product::query()
     </div>
 </header>
 @include('front.bookworm.header.aside')
+
+
+<script>
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+function removingClass(){
+    $('.titles').removeClass('d-none');
+}
+
+function addingClass(){
+    $('.titles').addClass('d-none');
+}
+
+
+
+</script>
+
+<style>
+.dropbtn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #3e8e41;
+}
+
+#myInput {
+  box-sizing: border-box;
+  background-image: url('searchicon.png');
+  background-position: 14px 12px;
+  background-repeat: no-repeat;
+  font-size: 16px;
+  padding: 14px 20px 12px 45px;
+  border: none;
+  border-bottom: 1px solid #ddd;
+}
+
+#myInput:focus {outline: 3px solid #ddd;}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f6f6f6;
+  min-width: 230px;
+  overflow: auto;
+  border: 1px solid #ddd;
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown a:hover {background-color: #ddd;}
+
+.show {display: block;}
+</style>
